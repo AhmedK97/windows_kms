a18f2b1ddb1a4a8c51d0a6517dc5f718dca50980
Ahmed Kamel <dx0.1997@gmail.com>
2025-01-01 23:53:17 +0200

    Refactor Vite configuration to dynamically include all CSS and JS files

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import fg from "fast-glob";
+
+const cssFiles = fg.sync("resources/css/*.css");
+const jsFiles = fg.sync("resources/js/*.js");
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [...cssFiles, ...jsFiles],
             refresh: true,
         }),
     ],
